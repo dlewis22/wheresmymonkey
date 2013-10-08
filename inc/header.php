1dcd4c347c990b2385f1d15a2ef6b616db40521a
David Lewis <dlewis22@gmail.com>
2013-10-08 10:20:51 -0700

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!doctype html>   
+	<head>   
+		<meta charset="utf-8">
+
+		<!-- the title tag should be 60 characters or less -->  
+		<title>wheresmymonkey.com</title>
+
+		<!-- meta description must be limited to 160 characters or less -->
+		<meta name="description" content="A social network game that connects real people in real life"> 
+		  
+		<meta name="author" content="David Lewis">
+	</head>
+	<body>
